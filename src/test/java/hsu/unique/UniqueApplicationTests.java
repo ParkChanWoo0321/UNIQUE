7ede95ed4f1ef97fab63b1abb5694e5728f235be
ParkChanWoo0321 <op9563_@naver.com>
2026-09-19 20:04:13 +0900

    Spring Boot 프로젝트 기본 구조 구성

---
+++ src/test/java/hsu/unique/UniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package hsu.unique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
